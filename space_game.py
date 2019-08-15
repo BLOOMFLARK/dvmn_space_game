f11bfdb6b970e3a9e80823c055a4c679a05d765a
Utkin Ilya Vladimirovich <i.utkin@tcsbank.ru>
2019-08-15 13:13:12 +0300

    add Border

--- space_game.py
+++ space_game.py
@@ -4,12 +4,20 @@ import time
 
 def draw(canvas):
     row, column = (5, 20)
-    canvas.addstr(row, column, 'Hello World')
-    canvas.refresh()
     canvas.border()
+    # no blinking cursor
+    curses.curs_set(False)
+    
+    # adds string on (x,y) = (row, column)
+    canvas.addstr(row, column, 'Hello World')
+
+    # make the string appear on the screen
+    canvas.refresh()
+
     time.sleep(3)
 
 
 if __name__ == '__main__':
     curses.update_lines_cols()
     curses.wrapper(draw)
+
