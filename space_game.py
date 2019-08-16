1acc88479bebe65659b0844aa2e69b0aef3ac4f6
Utkin Ilya Vladimirovich <i.utkin@tcsbank.ru>
2019-08-16 15:08:43 +0300

    moving rocket with borders

--- space_game.py
+++ space_game.py
@@ -2,11 +2,11 @@ import curses
 import time
 import asyncio
 import random
-from curses_tools import draw_frame
+from curses_tools import draw_frame, read_controls, get_frame_size
 
 
 async def blink(canvas, row, column, symbol='*'):
-    latency = random.randint(0, 10)
+    latency = random.randint(0, 100)
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
         latency = random.randint(0, 1)
@@ -53,14 +53,27 @@ async def animate_spaceship(canvas, row, column):
 '   '
   )
  ( )
-  ("""        
+  ("""
+    frame_rows, frame_columns = get_frame_size(frame1)
+    max_row, max_column = canvas.getmaxyx()
     while True:
+        rows_direction, columns_direction, space_pressed = read_controls(canvas)
+        
+        if row + rows_direction + frame_rows < max_row and -1 < row + rows_direction:
+            row += rows_direction
+        if column + columns_direction + frame_columns < max_column and 0 < column + columns_direction:
+            column += columns_direction    
+
+        
         draw_frame(canvas, row, column, frame1)
+        canvas.refresh()  
         await asyncio.sleep(0)
 
         draw_frame(canvas, row, column, frame1, negative=True)
         draw_frame(canvas, row, column, frame2)
-        await asyncio.sleep(0)
+        canvas.refresh()  
+        for _ in range(2):
+            await asyncio.sleep(0)
 
         draw_frame(canvas, row, column, frame2, negative=True)
 
@@ -71,6 +84,7 @@ def draw(canvas):
     TIC_TIMEOUT = 0.1
     curses.curs_set(False)
     canvas.border()
+    canvas.nodelay(True)
 
     max_y, max_x = canvas.getmaxyx()
     num_of_stars = random.randint(100, 200)
@@ -78,13 +92,12 @@ def draw(canvas):
     coroutines = []
     for _ in range(num_of_stars):
         star = random.choice(stars)
-        row = random.randint(1, max_y - 1)
-        column = random.randint(1, max_x - 1)
+        row = random.randint(1, max_y - 2)
+        column = random.randint(1, max_x - 2)
         coroutines.append(blink(canvas, row, column, symbol=star))
 
     blast = fire(canvas, round(max_y / 2), round(max_x / 2))
     rocket = animate_spaceship(canvas, round(max_y / 2), round(max_x / 2)) 
-    canvas.refresh()
 
     while True:
         time.sleep(TIC_TIMEOUT)
@@ -93,7 +106,8 @@ def draw(canvas):
             coroutine.send(None)
             canvas.refresh()
 
-        rocket.send(None)    
+        rocket.send(None)
+
         try:
             blast.send(None)
         except StopIteration:
@@ -101,7 +115,7 @@ def draw(canvas):
 
 
 
-async def fire(canvas, start_row, start_column, rows_speed=-0.3,  columns_speed=0):
+async def fire(canvas, start_row, start_column, rows_speed=-0.05,  columns_speed=0):
     row, column = start_row, start_column, 
 
     canvas.addstr(round(row), round(column), '*')
